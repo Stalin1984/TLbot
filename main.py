64e1194227c521a52b6b3cd55f2e520f225968ef
Stalin1984 <67471427+Stalin1984@users.noreply.github.com>
2021-09-16 23:00:57 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,79 +3,87 @@ import telebot
 from datetime import datetime
 import random
 
-tk = token
 
-tb = telebot.TeleBot(tk)
+while True:
+    
+    try:
 
-memes = [
-    'https://vk.com/photo6630825_305221390',
-    'https://vk.com/photo6630825_305221418',
-    'https://vk.com/photo6630825_305221409',
-    'https://vk.com/photo6630825_305221491',
-    'https://vk.com/photo6630825_319710575',
-    'https://vk.com/photo6630825_319710589'
-]
+        tk = token
 
-replies_time = ['скажи, а сколько времени?','скажи а сколько времени?',
-                'сколько времени?','скок времени?', 'который час?']
+        tb = telebot.TeleBot(tk)
 
-@tb.message_handler(commands=['start','help','myid'])
-def comands(message):
+        memes = [
+            'https://vk.com/photo6630825_305221390',
+            'https://vk.com/photo6630825_305221418',
+            'https://vk.com/photo6630825_305221409',
+            'https://vk.com/photo6630825_305221491',
+            'https://vk.com/photo6630825_319710575',
+            'https://vk.com/photo6630825_319710589'
+        ]
 
-    if message.text == '/start':
-        tb.send_message(message.chat.id, 'Привет, '+ str(message.from_user.first_name) + " " + str(message.from_user.last_name) + "!")
-    if message.text == '/myid':
-        tb.send_message(message.chat.id, "Твой id: " + str(message.from_user.username))
-    elif message.text == '/help':
+        replies_time = ['скажи, а сколько времени?','скажи а сколько времени?',
+                        'сколько времени?','скок времени?', 'который час?']
 
-        tb.send_message(message.chat.id,
-        "В чем проблема, дружище?\n"
-        "Вот список доступных команд:\n"
-        "Чтобы узнать время:\n"
-        "    Который час?\n"
-        "    Cкок времени?\n"
-        "    Cкажи, а сколько времени?\n"
-        "    Cколько времени?\n"
-        "Чтобы получить мемчик:\n"
-        "    Cкинь прикол\n"
-        "    Cкинь мемчик\n"
-        "Чтобы сделать правильный выбор:\n"
-        "    За кого мне голосовать?\n"
-        "    Путин или обама?")
-    else:
-        pass
+        @tb.message_handler(commands=['start','help','myid'])
+        def comands(message):
 
-@tb.message_handler(func=lambda m: True)
-def replies_to(message):
+            if message.text == '/start':
+                tb.send_message(message.chat.id, 'Привет, '+ str(message.from_user.first_name) + " " + str(message.from_user.last_name) + "!")
+            if message.text == '/myid':
+                tb.send_message(message.chat.id, "Твой id: " + str(message.from_user.username))
+            elif message.text == '/help':
 
-    if message.text[0] == '/':
-        tb.send_message(message.chat.id, 'Вы ввели несуществующую команду.')
+                tb.send_message(message.chat.id,
+                "В чем проблема, дружище?\n"
+                "Вот список доступных команд:\n"
+                "Чтобы узнать время:\n"
+                "    Который час?\n"
+                "    Cкок времени?\n"
+                "    Cкажи, а сколько времени?\n"
+                "    Cколько времени?\n"
+                "Чтобы получить мемчик:\n"
+                "    Cкинь прикол\n"
+                "    Cкинь мемчик\n"
+                "Чтобы сделать правильный выбор:\n"
+                "    За кого мне голосовать?\n"
+                "    Путин или обама?")
+            else:
+                pass
 
-    elif message.text.lower() in replies_time:
+        @tb.message_handler(func=lambda m: True)
+        def replies_to(message):
 
-        tm = datetime.now()
-        current_time = tm.strftime("%H:%M:%S")
-        tb.send_message(message.chat.id, str(current_time))
+            if message.text[0] == '/':
+                tb.send_message(message.chat.id, 'Вы ввели несуществующую команду.')
 
-    elif message.text.lower() in ['за кого мне голосовать?', 'путин или обама?']:
+            elif message.text.lower() in replies_time:
 
-        photo = 'https://ic.pics.livejournal.com/tipolog/9755416/989595/989595_original.jpg'
-        tb.reply_to(message, 'Не знаю как вы, а я на выборы никогда не ходил, но в этот раз буду головать за Ивана.')
-        tb.send_message(message.chat.id, 'Ивана в президенты!!!')
-        tb.send_photo(message.chat.id, photo)
+                tm = datetime.now()
+                current_time = tm.strftime("%H:%M:%S")
+                tb.send_message(message.chat.id, str(current_time))
 
-    elif message.text.lower() in ['скинь прикол', 'скинь мемчик']:
+            elif message.text.lower() in ['за кого мне голосовать?', 'путин или обама?']:
 
-        mem = random.choice(memes)
-        tb.send_photo(message.chat.id, mem)
+                photo = 'https://ic.pics.livejournal.com/tipolog/9755416/989595/989595_original.jpg'
+                tb.reply_to(message, 'Не знаю как вы, а я на выборы никогда не ходил, но в этот раз буду головать за Ивана.')
+                tb.send_message(message.chat.id, 'Ивана в президенты!!!')
+                tb.send_photo(message.chat.id, photo)
 
-    else:
+            elif message.text.lower() in ['скинь прикол', 'скинь мемчик']:
 
-        tb.send_message(message.chat.id,
+                mem = random.choice(memes)
+                tb.send_photo(message.chat.id, mem)
 
-        'Я вас не понял.'
-        'Возможно вы допустили ошибку в слове,'
-        'или такой команду еще не существет'
+            else:
 
-    )
-tb.polling()
+                tb.send_message(message.chat.id,
+
+                'Я вас не понял.'
+                'Возможно вы допустили ошибку в слове,'
+                'или такой команду еще не существет'
+
+            )
+        tb.polling()
+    except Exception as e:
+        time.sleep(1)
+        print(e)
